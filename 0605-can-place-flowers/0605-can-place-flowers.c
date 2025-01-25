c81ac0231ea7404c1cda887aa06158cd8573ba5a
Hunterwolf-SK <153583228+Hunterwolf-SK@users.noreply.github.com>
2025-01-25 23:12:07 +0530

    Time: 0 ms (100%), Space: 9.4 MB (46.36%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.c
+++ 0605-can-place-flowers/0605-can-place-flowers.c
@@ -1,12 +1,17 @@
-bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) {
+bool canPlaceFlowers(int* flowerbed, int flowerbedSize, int n) 
+{
     for (int i = 0; i < flowerbedSize; i++)
      {
-        if (flowerbed[i] == 0 && (i == 0 || flowerbed[i - 1] == 0) && 
+        if (flowerbed[i] == 0 &&
+            (i == 0 || flowerbed[i - 1] == 0) &&
             (i == flowerbedSize - 1 || flowerbed[i + 1] == 0)) 
+            
             {
             flowerbed[i] = 1;
-            --n;
+            n--;
         }
+    }
+    for (int i = 0; i < flowerbedSize; i++) {
         if (n <= 0) {
             return true;
         }
