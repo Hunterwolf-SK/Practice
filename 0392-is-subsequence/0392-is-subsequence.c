9bee8aeee2120054479ef136db8677e72a284b1a
Hunterwolf-SK <153583228+Hunterwolf-SK@users.noreply.github.com>
2025-01-25 23:24:19 +0530

    Time: 0 ms (100%), Space: 7.8 MB (95.4%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.c
+++ 0392-is-subsequence/0392-is-subsequence.c
@@ -1,3 +1,16 @@
-bool isSubsequence(char* s, char* t) {
-    
+bool isSubsequence(char* s, char* t) 
+{
+    int sl = strlen(s);
+    int tl = strlen(t);
+    int i = 0; 
+    int j = 0; 
+
+    while (i < sl && j < tl) {
+        if (s[i] == t[j]) {
+            i++; 
+        }
+        j++; 
+    }
+
+    return i == sl; 
 }
